7539e0c9b3e1f27d8eb62ededbe1b81448ddccd4
HichemMerniz <Hichembba97@gmail.com>
2019-02-10 21:02:29 +0100

    Tables Database

---
+++ lib/Models/place.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_database/firebase_database.dart';
+
+class place{
+  String id_place ;
+  String title_place ;
+  String discription_place ;
+  String position_place ;
+  double note_place ;
+  String image_place ;
+
+  place(this.title_place,this.discription_place);
+
+}
---
+++ lib/Models/user.dart
@@ -0,0 +1,15 @@
+import 'package:firebase_database/firebase_database.dart';
+
+class user {
+  String id_User ;
+  String first_name ;
+  String seconde_name ;
+  String username ;
+  int age ;
+  String country ;
+  String email ;
+  String passeword ;
+
+  user(this.first_name,this.seconde_name,this.email,this.username,);
+
+}
